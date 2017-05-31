3479f532332c48b274a4cd633603379741961053
王明初 <wangmingchu@wdai.com>
2017-05-31 17:31:22 +0800

    可以了

---
+++ src/test/java/com.wung.mybatis.dao/UserDaoTest.java
@@ -0,0 +1,82 @@
+package com.wung.mybatis.dao;
+
+import com.wung.mybatis.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.util.List;
+
+
+/**
+ *
+ * Created by wung on 2017/4/27.
+ */
+public class UserDaoTest {
+
+    private static SqlSessionFactory getSqlSessionFactory() {
+        SqlSessionFactory sessionFactory = null;
+        String config = "mybatis-config.xml";
+        try {
+            sessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader(config));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sessionFactory;
+    }
+
+    @Test
+    public void findById() {
+        SqlSessionFactory sessionFactory = getSqlSessionFactory();
+        SqlSession session = sessionFactory.openSession();
+
+        try {
+            UserDao userDaoMapping = session.getMapper(UserDao.class);
+            User user = userDaoMapping.findById(1);
+            System.out.println(user);
+            assert user != null;
+        } finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void findAllUsers() {
+        SqlSessionFactory sessionFactory = getSqlSessionFactory();
+        SqlSession session = sessionFactory.openSession();
+
+        try {
+            UserDao userDaoMapping = session.getMapper(UserDao.class);
+
+            List<User> users = userDaoMapping.findAllUsers();
+            assert users.size() > 0;
+        } finally {
+            session.close();
+        }
+    }
+
+    /**
+     * 修改后一定要 commit
+     */
+    @Test
+    public void insert() {
+        User user = new User();
+        user.setLoginName("jack");
+        user.setAge(29);
+
+        SqlSessionFactory sessionFactory = getSqlSessionFactory();
+        SqlSession session = sessionFactory.openSession();
+        try {
+            UserDao userDaoMapping = session.getMapper(UserDao.class);
+            userDaoMapping.insert(user);
+            session.commit();
+            assert user.getId() != null;
+        } finally {
+            session.close();
+        }
+    }
+
+}
